a6c96ca70ff26b2757ee76017c54909b39c6814b
Ian Rees <ian@ianrees.net>
2015-02-19 10:33:30 -0800

    Start of tests for gtfs reader

---
+++ onestop/test_gtfs.py
@@ -0,0 +1,16 @@
+"""geohash unit tests."""
+import unittest
+import os
+import json
+
+import gtfs
+
+class TestGTFSReader(unittest.TestCase):
+  def test_readcsv(self):
+    raise NotImplementedError
+    
+  def test_stops_centroid(self):
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    unittest.main()
