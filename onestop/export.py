341616ceef0ca70d6e72b1038bccdf765b69c7d7
Ian Rees <ian@ianrees.net>
2015-03-23 18:08:42 -0400

    Moved feature into OnestopFeed

--- onestop/export.py
+++ onestop/export.py
@@ -2,7 +2,9 @@
 import argparse
 import json
 import sys
+import os
 
+import util
 import registry
 import entities
 import mzgtfs.reader
@@ -19,41 +21,24 @@ if __name__ == "__main__":
     action='store_true')
   args = parser.parse_args()
   
-  r = registry.OnestopRegistry(args.path)
-  
+  # todo: load from registry, then update.
+  # r = registry.OnestopRegistry(args.path)
+  feed = entities.OnestopFeed.from_gtfs(
+    args.filename, 
+    name=args.feedid
+  )
+  print "Feed:", feed.onestop()
+  print "  Operators:", len(feed.operators())
+  print "  Routes:", len(feed.routes())
+  print "  Stops:", len(feed.stops())
+  if args.feed:
+    with open(os.path.join('feeds', '%s.json'%feed.onestop()), 'w') as f:
+      util.json_dump_pretty(feed.json(), f)
 
-  g = mzgtfs.reader.Reader(args.filename, feedid=args.feedid)
-  # Load and display information about agencies
-  for agency in g.agencies():
-    agency.preload()
-    oagency = entities.OnestopOperator.from_gtfs(agency)
-    print "Operator:", oagency.name()
-    print "  Routes:", len(oagency.routes())
-    if args.debug:
-      for route in oagency.routes():
-        print route
-    print "  Stops:", len(oagency.stops())
-    if args.debug:
-      for stop in oagency.stops():
-        print stop
-
-    # Export agency
+  for operator in feed.operators():
+    print "Operator:", operator.name()
+    print "  Routes:", len(operator.routes())
+    print "  Stops:", len(operator.stops())
     if args.agencies:
-      outfile = '%s.geojson'%oagency.onestop()
-      print "Writing: %s"%outfile
-      with open(outfile, 'w') as f:
-        json.dump(
-          oagency.geojson(), 
-          f, 
-          sort_keys=True, 
-          indent=4, 
-          separators=(',', ': ')
-        )
-
-  # Print basic information about feed
-  
-
-  # print "Feed:", feedid
-  # print "  Agencies:", len(g.agencies())
-  # print "  Routes:", len(g.routes())
-  # print "  Stops:", len(g.stops())
+      with open(os.path.join('operators', '%s.geojson'%operator.onestop()), 'w') as f:
+        util.json_dump_pretty(operator.json(), f)
