70bc6ccb5bc3f5f6838f21d67b644491b5482dc8
Ian Rees <ian@ianrees.net>
2015-03-24 04:26:16 -0400

    Bug fixes and automatically write output

--- onestop/export.py
+++ onestop/export.py
@@ -4,41 +4,80 @@ import json
 import sys
 import os
 
+import tempfile
+import urllib
+
 import util
 import registry
 import entities
 import mzgtfs.reader
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='GTFS to Agency')
-  parser.add_argument('filename', help='GTFS File')
-  parser.add_argument('--path', help='Onestop Registry path')
-  parser.add_argument('--agencies', help='Export agencies', action='store_true')
-  parser.add_argument('--feed', help='Export feed', action='store_true')
+  parser = argparse.ArgumentParser(description='Onestop Registry from GTFS.')
+  parser.add_argument('--filename', help='GTFS File')
+  parser.add_argument('--url', help='GTFS url')
   parser.add_argument('--feedid', help='Feed ID', default='unknown')
-  parser.add_argument('--debug', 
+  parser.add_argument('--onestop', help='Onestop Registry path', default='.')
+  parser.add_argument(
+    '--debug', 
     help='Show helpful debugging information', 
     action='store_true')
   args = parser.parse_args()
+
+  # Create dirs
+  for i in ['feeds', 'operators']:
+    try:
+      os.makedirs(i)
+    except OSError, e:
+      pass
   
-  # todo: load from registry, then update.
-  # r = registry.OnestopRegistry(args.path)
-  feed = entities.OnestopFeed.from_gtfs(
-    args.filename, 
-    name=args.feedid
-  )
-  print "Feed:", feed.onestop()
+  # Load from registry, then update.
+  carry = {}
+  carry['name'] = args.feedid
+  try:
+    r = registry.OnestopRegistry(args.onestop)
+    f = r.feed('f-%s'%args.feedid)
+  except IOError, e:
+    pass
+  else:
+    carry['url'] = f.url
+    carry['tags'] = f.tags
+    # Use previous url if no filename or url was provided.
+    args.url = args.url or f.url
+  
+  # Download GTFS feed.
+  filename = None
+  if args.filename:
+    filename = args.filename
+  elif args.url:
+    print "Downloading:", args.url
+    filename = tempfile.NamedTemporaryFile().name
+    urllib.urlretrieve(args.url, filename)
+    carry['url'] = args.url
+    print "...done"
+  else:
+    raise Exception("No filename or url provided.")
+    
+  # Load from GTFS.
+  feed = entities.OnestopFeed.from_gtfs(filename, debug=args.debug, **carry)
+
+  # Print basic feed information.
+  print "Feed:", 'f-%s'%args.feedid
   print "  Operators:", len(feed.operators())
   print "  Routes:", len(feed.routes())
   print "  Stops:", len(feed.stops())
-  if args.feed:
-    with open(os.path.join('feeds', '%s.json'%feed.onestop()), 'w') as f:
-      util.json_dump_pretty(feed.json(), f)
 
+  # Write out updated feed.
+  outfile = os.path.join('feeds', 'f-%s.json'%args.feedid)
+  with open(outfile, 'w') as f:
+    util.json_dump_pretty(feed.json(), f)
+  
+  # Print basic operator information.
   for operator in feed.operators():
     print "Operator:", operator.name()
     print "  Routes:", len(operator.routes())
     print "  Stops:", len(operator.stops())
-    if args.agencies:
-      with open(os.path.join('operators', '%s.geojson'%operator.onestop()), 'w') as f:
-        util.json_dump_pretty(operator.json(), f)
+    # Write out updated operators.
+    outfile = os.path.join('operators', '%s.geojson'%operator.onestop())
+    with open(outfile, 'w') as f:
+      util.json_dump_pretty(operator.json(), f)
