9e9c33958d6c921d07337bbb48bd20e407754db7
Ian Rees <ian@ianrees.net>
2015-03-25 03:47:09 -0400

    Updated to reflect changes

--- onestop/fetch.py
+++ onestop/fetch.py
@@ -2,23 +2,21 @@
 import onestop
 import argparse
 
+import registry
+
 def run():
   parser = argparse.ArgumentParser(description='Read Onestop Registry')
-  parser.add_argument('path', help='Path to Onestop Registry')
-  parser.add_argument('feeds', nargs='*', help='Fetch Onestop Feeds')
-  parser.add_argument('--prefix', help='Fetch Onestop Feeds with GeoHash Prefix.')
+  parser.add_argument('--onestop', help='Onestop Registry path', default='.')
+  parser.add_argument('feedids', nargs='*', help='Onestop Feed IDs')
   args = parser.parse_args()
+
   # Onestop Registry
-  registry = onestop.OnestopRegistry(path=args.path)    
-  # Use generator
-  if args.feeds:
-    feeds = (registry.load_feed(i) for i in args.feeds)
-  else:
-    feeds = registry.load_feeds()
-  # Fetch each feed
-  for feed in feeds:
-    print "Fetching feed %s: %s"%(feed.onestopId, feed.url)
-    feed.fetch('%s.zip'%feed.onestopId)
+  r = registry.OnestopRegistry(path=args.onestop)
+  feedids = args.feedids or r.feeds()
+  for feedid in feedids:
+    feed = r.feed(feedid)
+    filename = '%s.zip'%feedid
+    feed.fetch(filename)
     
 if __name__ == "__main__":
   run()
