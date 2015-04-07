fdcc2fcca8e966b8055da91d2d5fb039521b277f
Ian Rees <ian@ianrees.net>
2015-04-06 18:00:47 -0700

    Fixed url

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
   description='Transitland Onestop',
   author='Ian Rees',
   author_email='ian@mapzen.com',
-  url='http://mapzen.com/',
+  url='https://github.com/transitland/onestop-id-python-client',
   license='License :: OSI Approved :: MIT License',
-  packages=['onestop']
+  packages=['onestop'],
+  install_requires=['mzgtfs', 'mzgeohash'],
+  zip_safe=False,
+  # Include examples.
+  package_data = {
+    '': ['*.txt', '*.md', '*.zip']
+  }  
 )
