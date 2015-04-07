af1aaea786d30400684201aceb54870cd035ed17
Ian Rees <ian@ianrees.net>
2015-04-06 18:03:24 -0700

    Renamed sha1 to sha1file, better caching.

--- onestop/util.py
+++ onestop/util.py
@@ -4,11 +4,18 @@ import os
 import json
 import hashlib
 
-def download(url, filename, checksum=None):
-  # filename = tempfile.NamedTemporaryFile().name  
-  if os.path.exists(filename) and sha1(filename) == checksum:
-    print "Cached: %s"%(filename)
-    return filename
+def download(url, filename, sha1=None, cache=True):
+  if cache and os.path.exists(filename):
+    if sha1 and sha1file(filename) == sha1:
+      # Cached file, valid sha1 hash
+      print "Cached: %s (valid sha1)"%(filename)
+      return filename
+    elif sha1:
+      print "Cached, but incorrect signature."
+    else:
+      # Cached file, no sha1
+      print "Cached: %s (sha1 not checked)"%(filename)
+      return filename
   print "Downloading: %s -> %s"%(url, filename)
   urllib.urlretrieve(url, filename)
   return filename
@@ -22,8 +29,8 @@ def json_dump_pretty(data, f):
     separators=(',', ': ')
   )
 
-def sha1(filename, blocksize=65536):
-  """Return SHA1 Checksum of a file."""
+def sha1file(filename, blocksize=65536):
+  """Return SHA1 hash of a file."""
   h = hashlib.sha1()
   with open(filename, 'rb') as f:
       chunk = f.read(blocksize)
@@ -31,4 +38,10 @@ def sha1(filename, blocksize=65536):
           h.update(chunk)
           chunk = f.read(blocksize)
   return h.hexdigest()
-  
+
+def example_feed(feed='sample-feed.zip'):
+  return os.path.join(
+    os.path.dirname(__file__), 
+    'examples',
+    feed
+    )
