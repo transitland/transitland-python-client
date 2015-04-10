fd5e7861f4a5d9dd8b5e9cc877f8b598bd262767
Ian Rees <ian@ianrees.net>
2015-04-09 18:41:42 -0700

    Download some / all feeds.

--- onestop/fetch.py
+++ onestop/fetch.py
@@ -6,13 +6,16 @@ import registry
 
 def run():
   parser = argparse.ArgumentParser(description='Read Onestop Registry')
-  parser.add_argument('--onestop', help='Onestop Registry path', default='.')
   parser.add_argument('feedids', nargs='*', help='Onestop Feed IDs')
+  parser.add_argument('--onestop', help='Onestop Registry path', default='.')
+  parser.add_argument('--all', help='Update all feeds', action='store_true')
   args = parser.parse_args()
 
   # Onestop Registry
   r = registry.OnestopRegistry(path=args.onestop)
-  feedids = args.feedids or r.feeds()
+  feedids = args.feedids
+  if args.all:
+    feedids = r.feeds()
   for feedid in feedids:
     feed = r.feed(feedid)
     filename = '%s.zip'%feedid
