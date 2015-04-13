3ab2b0af9bd2613bfaf03de212a0ae6df6f1dfe2
Ian Rees <ian@ianrees.net>
2015-04-13 01:37:58 -0700

    Cosmetic

--- onestop/geom.py
+++ onestop/geom.py
@@ -4,15 +4,16 @@ import mzgeohash
 import errors
 
 def geohash_features(features):
+  """mzgeohash.neighborsfit on a list of features that implement point()."""
   # Filter stops without valid coordinates...
   points = [feature.point() for feature in features if feature.point()]
   if not points:
     raise errors.OnestopNoPoints("Not enough points.")
-  c = centroid_points(points)
+  c = centroid(points)
   return mzgeohash.neighborsfit(c, points)
   
-def centroid_points(points):
-  """Return the lon,lat centroid for features."""
+def centroid(points):
+  """Return the lon,lat simple geometric centroid for features."""
   # Todo: Geographic center, or simple average?
   x = sum(i[0] for i in points)
   y = sum(i[1] for i in points)
