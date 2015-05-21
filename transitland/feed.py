d4eb38df4ab83504d2961dfe598088794bcab302
Ian Rees <ian@ianrees.net>
2015-05-21 12:48:52 -0700

    Improved Feed.from_gtfs loader.

--- transitland/feed.py
+++ transitland/feed.py
@@ -6,6 +6,8 @@ import util
 import errors
 from entity import Entity
 from operator import Operator
+from stop import Stop
+from route import Route
 
 class Feed(Entity):
   """Transitland Feed Entity."""
@@ -36,7 +38,7 @@ class Feed(Entity):
       return filename
     util.download(self.url(), filename)
     if verify and not self.verify_sha1(filename):
-      raise errors.InvalidChecksumError("Incorrect SHA1 Checksum: %s, expected %s"%(
+      raise errors.InvalidChecksumError("Incorrect checksum: %s, expected %s"%(
         util.sha1file(filename),
         self.sha1()
       ))
@@ -50,14 +52,83 @@ class Feed(Entity):
       gtfs_feed.preload()
     except AttributeError, e:
       pass
+    for agency in gtfs_feed.agencies():
+      try:
+        agency.preload()
+      except AttributeError, e:
+        pass
+    
     # Create feed
     kw['sha1'] = util.sha1file(gtfs_feed.filename)
     kw['geohash'] = geom.geohash_features(gtfs_feed.stops())
     feed = cls(**kw)
-    # Load and display information about agencies
-    for agency in gtfs_feed.agencies():
-      oagency = Operator.from_gtfs(agency, feedid=feedid, debug=debug)
-      feed.add_child(oagency)
+
+    # Create TL Stops
+    stops = {}
+    # Sort; process all stations first.
+    order = []
+    order += sorted(filter(lambda x:x.location_type()==1, gtfs_feed.stops()), key=lambda x:x.id())
+    order += sorted(filter(lambda x:x.location_type()!=1, gtfs_feed.stops()), key=lambda x:x.id())
+    for gtfs_stop in order:
+      stop = Stop(
+        name=gtfs_stop.name(),
+        geometry=gtfs_stop.geometry()
+      )
+      # Add to TL Stops
+      key = stop.onestop()
+      parent = gtfs_stop.get('parent_station')
+      if parent:
+        # merge into parent station
+        stop = gtfs_feed.stop(parent)._tl
+      elif key in stops:
+        # merge into matching onestop id
+        stop = stops[key]
+      else:
+        # new stop
+        stops[key] = stop
+      # Add identifiers and tags
+      gtfs_stop._tl = stop
+      stop.add_identifier(gtfs_stop.feedid(feedid))
+      stop.add_tags(gtfs_stop.data._asdict())
+    
+    # Create TL Routes
+    routes = {}
+    for gtfs_route in gtfs_feed.routes():
+      if not gtfs_route.stops():
+        print "No stops:", gtfs_route.name()
+        continue
+      route = Route(
+        name=gtfs_route.name(),
+        geometry=gtfs_route.geometry()
+      )
+      # Link to TL Stops
+      for gtfs_stop in gtfs_route.stops():
+        route.add_child(gtfs_stop._tl)
+      # Cache
+      key = route.onestop()
+      if key not in routes:
+        routes[key] = route
+      else:
+        route = routes[key]
+        print "merging route...", route, route.identifiers()
+      # Maintain reference to GTFS Route
+      gtfs_route._tl = route
+      route.add_identifier(gtfs_route.feedid(feedid))
+      route.add_tags(gtfs_route.data._asdict())
+        
+    for gtfs_agency in gtfs_feed.agencies():
+      agency = Operator(
+        name=gtfs_agency.name(),
+        geometry=gtfs_agency.geometry()
+      )
+      agency.add_identifier(gtfs_agency.feedid(feedid))
+      agency.add_tags(gtfs_agency.data._asdict())      
+      for gtfs_route in gtfs_agency.routes():
+        r = getattr(gtfs_route, '_tl', None)
+        if not r:
+          continue
+        agency.add_child(r)
+      feed.add_child(agency)
     return feed
     
   def json(self):
--- transitland/operator.py
+++ transitland/operator.py
@@ -1,6 +1,4 @@
 """Operator Entity."""
-import collections
-
 import geom
 import util
 import errors
@@ -26,68 +24,6 @@ class Operator(Entity):
     for i in self.stops():
       i.data[key] = i.make_onestop()
     
-  # Load / dump
-  @classmethod
-  def from_gtfs(cls, gtfs_agency, feedid='f-0-unknown', debug=False):
-    """Create Operator from a GTFS Agency."""
-    # Backwards compat.
-    try:
-      gtfs_agency.preload()
-    except AttributeError, e:
-      pass
-    route_counter = collections.defaultdict(int)
-    agency = cls(
-      name=gtfs_agency.name(),
-      geometry=gtfs_agency.geometry()
-    )
-    agency.add_identifier(gtfs_agency.feedid(feedid))
-    agency.add_tags(gtfs_agency.data._asdict())
-    # Group stops
-    stops = {}
-    for i in gtfs_agency.stops():
-      stop = Stop(
-        name=i.name(), 
-        geometry=i.geometry()
-      )
-      key = stop.onestop()
-      if key not in stops:
-        stops[key] = stop
-      # Hack to maintain ref to stop
-      i._tl_parent = stops[key]
-      stops[key].add_identifier(i.feedid(feedid))
-      stops[key].add_tags(i.data._asdict())
-    # Routes
-    routes = {}
-    for i in gtfs_agency.routes():
-      route = Route(
-        name=i.name(),
-        geometry=i.geometry()
-      )
-      if not i.stops():
-        if debug: # pragma: no cover
-          print "Yikes! No stops! Skipping this route." # pragma: no cover
-        continue
-      for j in i.stops():
-        route.pclink(route, j._tl_parent)
-      key = route.onestop()
-      if key in routes:
-        if debug: # pragma: no cover
-          print "Route already exists, setting temp fix..."
-        route_counter[key] += 1
-        route.data['name'] = '%s~%s'%(route.data['name'], route_counter[key])
-        route.data['onestop'] = None
-        key = route.onestop()
-        if debug: # pragma: no cover
-          print "set key to:", key
-        assert key not in routes
-      route.pclink(agency, route)
-      route.add_identifier(i.feedid(feedid))
-      route.add_tags(i.data._asdict())
-      routes[key] = route
-    # Return agency
-    agency._cache_onestop()
-    return agency
-
   @classmethod
   def from_json(cls, data):
     """Load Operator from GeoJSON."""
